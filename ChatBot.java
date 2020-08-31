f3add8e88c29255e2392649b96d093a201f6496b
MattiasITHS <mattias.hovde@iths.se>
2020-08-31 10:37:01 +0100

    Updated code in ChatBot

--- ChatBot.java
+++ ChatBot.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
 
 public class ChatBot{
+  public static Scanner input = new Scanner (System.in);
   public static void main (String[] args){
     Scanner input = new Scanner (System.in);
     //objekttyp ,namn på objekt //Nytt objekt
-
-    System.out.println("Hej vad heter du?");
+    greetings();
+    job();
+  }
+  public static void greetings(){
+    System.out.println("Hej, vad heter du?");
     String name = input.nextLine();
     System.out.println("Hej "+name);
-    System.out.println("Hur gammal är du och vad är ditt favoritdjur?");
-
-    int age = input.nextInt();
-    String favoriteAnimal = input.next();
-    System.out.println("Du är "+age+" år gammal och ditt favoritdjur är "+favoriteAnimal);
+  }
+  public static void job(){
+    System.out.println("Vad studerar du till?");
+    String job = input.nextLine();
+    System.out.println("Du studerar till "+job+", va roligt!");
   }
 }
