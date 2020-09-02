066cc97b3f486e5a86db8ffe8c53c996088589db
MattiasITHS <mattias.hovde@iths.se>
2020-09-02 21:33:30 +0100

    Added break; and prints

--- isAuthorised.java
+++ isAuthorised.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
           System.out.println("Lucky one, du lyckades på sista försöket");
         }else if (authorised){
           System.out.println("Grattis! du har matat in rätt kod!");
+          break;
       } else {
         System.out.println("FEL! Testa igen.");
         }
@@ -17,8 +18,11 @@ import java.util.Scanner;
     }
     public static boolean isAuthorised(){
       System.out.println("Mata in 3 st lösenord");
+      System.out.println("Lösenord1:");
       String guess1 = input.nextLine();
+      System.out.println("Lösenord2:");
       String guess2 = input.nextLine();
+      System.out.println("Lösenord3:");
       String guess3 = input.nextLine();
 
       return guess1.equals("piggy") && guess2.equals("bark") && guess3.equals("snuff")
