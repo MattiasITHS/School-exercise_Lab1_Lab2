56f142aba661c3b6ba3c72fb8c92a75ed6d3407f
MattiasITHS <mattias.hovde@iths.se>
2020-09-02 21:25:06 +0100

    Finished the task

--- isAuthorised.java
+++ isAuthorised.java
@@ -4,16 +4,28 @@ import java.util.Scanner;
     public static void main(String [] args){
 
 
-      isAuthorised();
+      for(int i = 0; i < 5;i++){
+        boolean authorised = isAuthorised();
+        if (authorised && i == 4){
+          System.out.println("Lucky one, du lyckades på sista försöket");
+        }else if (authorised){
+          System.out.println("Grattis! du har matat in rätt kod!");
+      } else {
+        System.out.println("FEL! Testa igen.");
+        }
+      }
     }
     public static boolean isAuthorised(){
       System.out.println("Mata in 3 st lösenord");
       String guess1 = input.nextLine();
       String guess2 = input.nextLine();
       String guess3 = input.nextLine();
-      String answer1 = "piggy";
-      String answer2 = "bark";
-      String answer3 = "snuff";
 
+      return guess1.equals("piggy") && guess2.equals("bark") && guess3.equals("snuff")
+      ||guess1.equals("bark") && guess2.equals("snuff") && guess3.equals("piggy")
+      ||guess1.equals("snuff") && guess2.equals("piggy") && guess3.equals("bark")
+      ||guess1.equals("piggy") && guess2.equals("snuff") && guess3.equals("bark")
+      ||guess1.equals("bark") && guess2.equals("piggy") && guess3.equals("snuff")
+      ||guess1.equals("snuff") && guess2.equals("bark") && guess3.equals("piggy");
     }
   }
