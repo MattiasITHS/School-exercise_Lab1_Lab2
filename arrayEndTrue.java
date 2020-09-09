6e8d82ad0f7fab5409b82ab13087e90472042773
MattiasITHS <mattias.hovde@iths.se>
2020-09-09 13:50:31 +0100

    Adding java task

---
+++ arrayEndTrue.java
@@ -0,0 +1,14 @@
+public class arrayEndTrue{
+  public static void main(String[]args){
+
+    endTrue(5);
+
+  }
+  public static boolean[] endTrue(int a){
+    boolean[] array = new boolean[a];
+    System.out.println(array.length);
+
+    return array;
+
+  }
+}
