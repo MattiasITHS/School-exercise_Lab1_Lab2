73ff780a5e07146067382a264385513c82ba6be5
MattiasITHS <mattias.hovde@iths.se>
2020-10-18 13:33:20 +0100

    Work in prog

--- SimpleDiceGame.java
+++ SimpleDiceGame.java
@@ -4,40 +4,38 @@ public class SimpleDiceGame{
   public static Scanner input = new Scanner(System.in);
   public static void main(String[]args){
 
-    ArrayList<Player> names = initialize();
+    ArrayList<Player> players = initialize();
+    for(int i = 0; i < players.size();i++){
+      System.out.println("Players; " + players.get(i).getPlayerName());
+    }
 
-    System.out.println(names);
-
-
+//TODO takeTurn, spelare 1, gissa värde, rulla tärning, få ett värde, stämmer gissning med värdet på tärning == vinst
   }
-  private static ArrayList<Player> initialize(){
 
-    String name;
-    ArrayList<Player> names = new ArrayList<Player>();
-    ArrayList<Die> dice = new ArrayList<Die>();
+
+  private static ArrayList<Player> initialize(){
+    ArrayList<Player> players = new ArrayList<Player>();
 
     System.out.println("How many players are playing?");
     int numberOfPlayers = input.nextInt();
 
     System.out.println("How many dice should each player have?");
     int numberOfDice = input.nextInt();
-      for(int i = 0; i < dice.size();i++){
-          dice.add(new Die(6));
-        }
+    System.out.println("How many sides shall the dice have?");
+    int numberOfSides = input.nextInt();
 
     System.out.println("Whats the name of the players?");
-    for(int i = 0; i <= numberOfPlayers; i++){
-      name = input.nextLine();
-      names.add(new Player(name));
+    for(int i = 0; i < numberOfPlayers; i++){
+      players.add(new Player(input.next()));
+      for(int j = 0; j < numberOfDice;j++){
+        players.get(i).addDie(numberOfSides);
+      }
     }
-    return (names);
+    return players;
   }
 
   private static void takeTurn(ArrayList<Player>players){
-    ArrayList<Player> names = new ArrayList<Player>();
-
-      for(int i = 0; i < names.size(); i++){
-        System.out.println(names.get(i).getPlayerName());
+      for(int i = 0; i < players.size(); i++){
       //   for (int k = 0; k < dice.size(); k++){
       //
       // }
