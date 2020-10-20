65a2878dfd33923cd69066d4581ba190707d952b
MattiasITHS <mattias.hovde@iths.se>
2020-10-20 14:14:50 +0100

    Work in getWinners method

--- SimpleDiceGame.java
+++ SimpleDiceGame.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SimpleDiceGame{
+  final static int GAME_ROUNDS = 5;
   public static Scanner input = new Scanner(System.in);
   public static void main(String[]args){
-
     ArrayList<Player> players = initialize();
-    for(int i = 0; i < players.size();i++){
-      System.out.println("Players; " + players.get(i).getPlayerName());
+    for(int i = 0; i < 2;i++){
       takeTurn(players);
     }
+    for(int i = 0; i < players.size();i++){
+      getWinners(players);
+      System.out.println(players.get(i).getPlayerName() + " score: " + players.get(i).getScore());
+    }
+
+
   }
 
 
@@ -31,20 +41,47 @@ public class SimpleDiceGame{
     }
     return players;
   }
-  //TODO printa value tot av en omgång, sum = players.get(i).getDieValue();
 
   private static void takeTurn(ArrayList<Player>players){
-    int score;
+    int guessScore;
     // !gå igenom listan med spelare
     for(int i = 0; i < players.size(); i++){
-      //rulla spelarens tärning/ar
-      System.out.println("Guess your score: ");
-      score = input.nextInt();
+      //!rulla spelarens tärning/ar
+      //!gissa ett värde
+      System.out.println("Player; " + players.get(i).getPlayerName());
+      System.out.println(players.get(i).getPlayerName() + " guess your score: ");
+      guessScore = input.nextInt();
       players.get(i).rollDice();
-      //gissa ett värde
-      //få ett värde
+      //!få ett värde
       System.out.println("The value of your roll are: " + players.get(i).getDieValue());
-
+      // öka spelarens poäng om gissning är rätt
+      if(guessScore==players.get(i).getDieValue()){
+        System.out.println("SCOOOOOOORE!");
+        players.get(i).increaseScore();
+      } else {
+        System.out.println("No score this round!");
+      }
     }
+  }
+  private static ArrayList<Player> getWinners(ArrayList<Player>players){
+    //ta emot listan
+    // returnera lista med vinnare
+    ArrayList<Player> winners = new ArrayList<Player>();
+    int maxScore = -1; //Assuming it is not possible to have negative points
+
+    for (int i = 0; i < players.size(); i++){
+      int score = players.get(i).getScore();
+      if (score >= maxScore) {
+        if (score > maxScore) {
+            winners.clear();
+          }
+        maxScore = score;
+        winners.add(players.get(i));
+      }
+    // vinnare == mest poäng
+      // jämföra poäng mellan spelare i arraylistan??
+    // lista kan innehålla fler vinnare, lika många poäng
+    }
+    return winners;
   }
 }
