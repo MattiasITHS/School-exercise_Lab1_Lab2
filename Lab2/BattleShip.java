ef9474321b4e9be5323729756052198deb5f3273
MattiasITHS <mattias.hovde@iths.se>
2020-10-27 07:28:16 +0100

    Work in prog

--- Lab2/BattleShip.java
+++ Lab2/BattleShip.java
@@ -1,6 +1,10 @@
-public class BattleShip{
-  public static void main (String[] args){
-    Board game = new Board(5,5);
+import java.util.Scanner;
 
+public class BattleShip{
+  public static Scanner input = new Scanner(System.in);
+  public static void main (String[] args){
+
+    Board b = new Board(10,10);
+    b.show();
   }
 }
--- Lab2/Board.java
+++ Lab2/Board.java
@@ -2,15 +2,25 @@ import java.util.Scanner;
 
 class Board{
 
-
   Scanner input = new Scanner(System.in);
-  private char [][] box;
+
+  private char[][] board;
   public Board(int rowNum, int columnNum){
-    this.box = new char [rowNum][columnNum];
+    this.board = new char[rowNum][columnNum];
+
   }
 
-  public static char[][] show(){
-   return null;
+  public void show(){
+    for (int i = 0; i < board.length; i++){
+      System.out.print(i + "[ ]");
+      for (int j = 0; j < board[i].length; j++){
+	     board[i][j] = ' ';
+       // System.out.println(board[i][j] + "");
+     }
+     System.out.print("[ ]" + i);
+       System.out.println();
+       //TODO få 10x10 att printa..
+    }
   }
   public void set(int i, int j){
 
